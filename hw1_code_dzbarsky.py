4aa3e147a9eef4a1b4252b6354251a036768d8f4
David Zbarsky <dzbarsky@gmail.com>
2013-09-16 22:44:08 -0400

    get all files

--- hw1_code_dzbarsky.py
+++ hw1_code_dzbarsky.py
@@ -10,22 +10,21 @@ Starbucks_root = 'corpus/starbucks'
 def get_sub_directories(directory):
     subdirs = set()
     for file in PlaintextCorpusReader(directory, '.*').fileids():
-        subdirs.add(file[0:file.find('/')])
+        index = file.find('/')
+        if index > 0:
+            subdirs.add(file[0:index])
     return list(subdirs)
 
 def get_all_files(directory):
     files = []
-    print "TESTING\n\n"
-    #print PlaintextCorpusReader(directory, '.txt').fileids()
-    #files.extend(PlaintextCorpusReader(directory, '.txt').fileids())
-    print files
+    files.extend(PlaintextCorpusReader(directory, '.*').fileids())
     for subdir in get_sub_directories(directory):
         files.extend(get_all_files(directory + "/" + subdir))
     return files
 
 def main():
     print get_sub_directories(Corpus_root)
-    print get_all_files(Corpus_root)
+    print get_all_files(Starbucks_root)
 
 if __name__ == "__main__":
     main()
