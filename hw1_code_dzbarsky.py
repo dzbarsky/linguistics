74bc0f0ccb7e9442e027347af6aabf2753eff9c7
David Zbarsky <dzbarsky@gmail.com>
2013-09-16 22:39:24 -0400

    make subdirs

--- hw1_code_dzbarsky.py
+++ hw1_code_dzbarsky.py
@@ -3,13 +3,29 @@
 
 from nltk.corpus import PlaintextCorpusReader
 
-Corpus_root = '/home1/c/cis530/hw1/data/corpus'
-Mixed_root = '/home1/c/cis530/hw1/data/mixed'
-
-#files_all = PlaintextCorpusReader(Starbucks_root, '.*')
+Corpus_root = 'corpus'
+Mixed_root = 'mixed'
+Starbucks_root = 'corpus/starbucks'
 
 def get_sub_directories(directory):
-    files_all = PlaintextCorpusReader(directory, '.*')
-    print files_all
+    subdirs = set()
+    for file in PlaintextCorpusReader(directory, '.*').fileids():
+        subdirs.add(file[0:file.find('/')])
+    return list(subdirs)
 
+def get_all_files(directory):
+    files = []
+    print "TESTING\n\n"
+    #print PlaintextCorpusReader(directory, '.txt').fileids()
+    #files.extend(PlaintextCorpusReader(directory, '.txt').fileids())
+    print files
+    for subdir in get_sub_directories(directory):
+        files.extend(get_all_files(directory + "/" + subdir))
+    return files
 
+def main():
+    print get_sub_directories(Corpus_root)
+    print get_all_files(Corpus_root)
+
+if __name__ == "__main__":
+    main()
