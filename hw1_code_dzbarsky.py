bea62a9755d9d8c12c5df39b1dd0bb792c32e3dd
David Zbarsky <dzbarsky@gmail.com>
2013-09-17 22:03:37 -0400

    Fix/optimize all the things

--- hw1_code_dzbarsky.py
+++ hw1_code_dzbarsky.py
@@ -23,7 +23,7 @@ def get_all_files(directory):
 
 def load_file_sentences(filepath):
     index = filepath.rfind('/')
-    dir = filepath[0:index]
+    dir = filepath[:index]
     filepath = filepath[index + 1:]
     return sent_tokenize(PlaintextCorpusReader(dir, filepath).raw().lower())
 
@@ -46,19 +46,18 @@ def load_collection_tokens(directory):
     return tokens
 
 def get_tf(path):
-    map = dict()
     if path.find('/') >= 0:
         tokens = load_collection_tokens(path)
     else:
         tokens = load_file_tokens(path)
+
+    count = 1.0/len(tokens)
+    map = dict()
     for token in tokens:
        if token in map:
-           map[token] += 1
+           map[token] += count
        else:
-           map[token] = 1
-    for token in map.keys():
-        pass
-        map[token] /= float(len(tokens))
+           map[token] = count
     return map
 
 def get_idf(directory):
@@ -66,25 +65,21 @@ def get_idf(directory):
     files = get_all_files(directory)
     for file in files:
         docs.append(load_file_tokens(directory + '/' + file))
-    print "DONE LOADING"
+
     map = dict()
     for doc in docs:
-        print doc
-        for token in doc:
+        for token in set(doc):
             if token in map:
-                continue
-            occurences = 0
-            for doc2 in docs:
-                if token in doc2:
-                    occurences += 1
-            map[token] = occurences
+                map[token] += 1
+            else:
+                map[token] = 1.0
 
     for token in map.keys():
         map[token] = math.log(len(files)/map[token])
 
     return map
 
-def get_tf_idf(dict1, dict2, k):
+def get_tf_idf_words(dict1, dict2, k):
     tuples = []
     for term in dict1:
         tuples.append((term, dict1[term] * dict2[term]))
@@ -98,16 +93,14 @@ def get_tf_idf(dict1, dict2, k):
 
 def main():
     #print get_sub_directories(Corpus_root)
-    #print get_all_files(Starbucks_root)
+    #print get_all_files(Corpus_root)
     #print load_file_sentences(Starbucks_root + '/118990300.txt')
     #print load_collection_sentences(Starbucks_root)
     #print load_file_tokens(Starbucks_root + '/118990300.txt')
     #print load_collection_tokens(Starbucks_root)
-    dict1 = get_tf(Heinz_root)
-    print dict1
-    dict2 = get_idf(Corpus_root)
-    print dict2
-    print get_tf_idf(dict1, dict2, 10)
+    #dict1 = get_tf(Heinz_root)
+    #dict2 = get_idf(Corpus_root)
+    #print get_tf_idf_words(dict1, dict2, 10)
 
 
 if __name__ == "__main__":
